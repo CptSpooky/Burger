f2bfb83d713053f52c77646a968143b05d0fb540
CptSpooky <legacyofthedonut@gmail.com>
2020-09-22 21:56:21 -0400

    schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Durba Curda Wibba Chings');
+INSERT INTO burgers (burger_name) VALUES ('Hongy Bunches of Chungo');
+INSERT INTO burgers (burger_name) VALUES ('Freef';
