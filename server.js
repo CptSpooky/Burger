2c7547d2e7a8fcf6bfc1226e3a554a6fee7504aa
CptSpooky <legacyofthedonut@gmail.com>
2020-09-26 17:24:03 -0400

    server port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const exphbs = require("express-handlebars");
 
 const app = express();
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 3306;
 
 // express handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
